56d2970d7a87aef25abb8face81db37c344b6020
Martin Lehoux <martin@lehoux.net>
2020-06-10 23:02:36 -0700

    feat - Alt vs Dist chart

--- apps/track/views.py
+++ apps/track/views.py
@@ -1,6 +1,8 @@
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from plotly.offline import plot
+from plotly.graph_objs import Scatter, Layout, Figure
 
 from .models import Track
 from .forms import TrackCreateForm
@@ -23,3 +25,26 @@ class TrackCreateView(LoginRequiredMixin, generic.CreateView):
 
 class TrackDetailView(generic.DetailView):
     model = Track
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        track = self.get_object()
+        alt_vs_dist_fig = Figure(
+            data=[
+                Scatter(
+                    x=list(track.point_set.values_list("dist", flat=True)),
+                    y=list(track.point_set.values_list("alt", flat=True)),
+                    mode="lines",
+                    name=track.name,
+                )
+            ],
+            layout=Layout(
+                title=track.name,
+                xaxis=dict(title="Distance (meters)"),
+                yaxis=dict(title="Altitude (meters)"),
+                margin=dict(r=0, l=0, t=0, b=0),
+            ),
+        )
+        alt_vs_dist = plot(alt_vs_dist_fig, output_type="div", include_plotlyjs=False,)
+        context.update({"alt_vs_dist": alt_vs_dist})
+        return context
