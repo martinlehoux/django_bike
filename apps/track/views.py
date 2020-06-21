0d6cf225f33a812991733e8d2ae9ab05f6e3134f
Martin Lehoux <martin@lehoux.net>
2020-06-20 22:21:11 -0700

    feat - Chart view

--- apps/track/views.py
+++ apps/track/views.py
@@ -3,11 +3,10 @@ from django.views import generic
 from django.db.models import Q
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
-from plotly.offline import plot
-from plotly import graph_objs as go
 
-from .models import Track, TrackData, smoother, TrackStat
+from .models import Track, TrackStat
 from .forms import TrackCreateForm
+from . import charts
 
 
 class TrackListView(generic.ListView):
@@ -41,67 +40,14 @@ class TrackDetailView(generic.DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         track = self.get_object()
-        track_data = TrackData(track)
-        alt_vs_dist_fig = go.Figure(
-            data=[
-                go.Scatter(
-                    x=track_data.dist(),
-                    y=smoother(track_data.alt()),
-                    mode="lines",
-                    name="Altitude",
-                ),
-            ],
-            layout=go.Layout(
-                title=track.name,
-                xaxis=dict(title="Distance (km)"),
-                yaxis=dict(title="Altitude (m)"),
-                margin=dict(r=0, l=0, t=0, b=0),
-            ),
-        )
-        slope_vs_dist_fig = go.Figure(
-            data=[
-                go.Scatter(
-                    x=track_data.dist(),
-                    y=smoother(track_data.slope()),
-                    mode="lines",
-                    name="Slope",
-                )
-            ],
-            layout=go.Layout(
-                title=track.name,
-                xaxis=dict(title="Distance (km)"),
-                yaxis=dict(title="Slope (%)"),
-                margin=dict(r=0, l=0, t=0, b=0),
-            ),
-        )
-        speed_vs_dist_fig = go.Figure(
-            data=[
-                go.Scatter(
-                    x=track_data.dist(),
-                    y=smoother(track_data.speed()),
-                    mode="lines",
-                    name="Speed",
-                )
-            ],
-            layout=go.Layout(
-                title=track.name,
-                xaxis=dict(title="Distance (km)"),
-                yaxis=dict(title="Speed (km/h)"),
-                margin=dict(r=0, l=0, t=0, b=0),
-            ),
-        )
         context.update(
             {
                 "track_stat": TrackStat(track),
-                "alt_vs_dist": plot(
-                    alt_vs_dist_fig, output_type="div", include_plotlyjs=False
-                ),
-                "slope_vs_dist": plot(
-                    slope_vs_dist_fig, output_type="div", include_plotlyjs=False
-                ),
-                "speed_vs_dist": plot(
-                    speed_vs_dist_fig, output_type="div", include_plotlyjs=False
-                ),
+                "charts": [
+                    charts.AltVSDistChart(track).plot(),
+                    charts.SlopeVSDistChart(track).plot(),
+                    charts.SpeedVSDistChart(track).plot(),
+                ],
             }
         )
         return context
