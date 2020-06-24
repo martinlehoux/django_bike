298fd8e58e0a46d764f69e983779ebc9f5b2819f
Martin Lehoux <martin@lehoux.net>
2020-06-23 23:58:35 -0700

    feat - Improve workflow and design for auth views

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -5,5 +5,20 @@ from . import views
 
 urlpatterns = [
     path("profile/", views.ProfileView.as_view(), name="profile"),
+    path(
+        "password_change/done/",
+        views.PasswordChangeDoneView.as_view(),
+        name="password_change_done",
+    ),
+    path(
+        "password_reset/done/",
+        views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/done/",
+        views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
     path("", include("django.contrib.auth.urls")),
 ]
--- apps/account/views.py
+++ apps/account/views.py
@@ -1,9 +1,46 @@
+from django.shortcuts import redirect
 from django.views.generic import DetailView
 from django.contrib.auth import get_user, get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import (
+    PasswordChangeDoneView,
+    PasswordResetDoneView,
+    PasswordResetCompleteView,
+)
+from django.contrib import messages
+
 User = get_user_model()
+
+
 class ProfileView(LoginRequiredMixin, DetailView):
     model = User
 
     def get_object(self, queryset=None):
         return get_user(self.request)
+
+
+class PasswordChangeDoneView(PasswordChangeDoneView):
+    def dispatch(self, request, *args, **kwargs):
+        messages.success(request, "Your password was changed successfully.")
+        return redirect("profile")
+
+
+class PasswordResetDoneView(PasswordResetDoneView):
+    def dispatch(self, request, *args, **kwargs):
+        messages.info(
+            request,
+            (
+                "We’ve emailed you instructions for setting your password, "
+                "if an account exists with the email you entered. You should "
+                "receive them shortly. If you don’t receive an email, please "
+                "make sure you’ve entered the address you registered with, and "
+                "check your spam folder."
+            ),
+        )
+        return redirect("login")
+
+
+class PasswordResetCompleteView(PasswordResetCompleteView):
+    def dispatch(self, request, *args, **kwargs):
+        messages.success(request, "Password reset complete")
+        return redirect("login")
