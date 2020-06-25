28e2cfa2197e1b92c19e8eb2206caa54bce1200f
Martin Lehoux <martin@lehoux.net>
2020-06-25 00:36:31 -0700

    fix - In prod, use static root for nginx

--- django_bike/settings.py
+++ django_bike/settings.py
@@ -94,7 +94,10 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = ["static"]
+if DEBUG:
+    STATICFILES_DIRS = ["static"]
+else:
+    STATIC_ROOT = "static/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = "media/"
 
