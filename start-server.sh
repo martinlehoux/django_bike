d11145019e2244ad9084d9ba2cf2969b61412035
Martin Lehoux <martin@lehoux.net>
2020-06-25 01:51:15 -0700

    fix - No clear static files

--- start-server.sh
+++ start-server.sh
@@ -3,6 +3,6 @@ do
   sleep 1
 done
 python manage.py migrate
-python manage.py collectstatic --no-input --clear
+python manage.py collectstatic --no-input
 python manage.py runserver 0.0.0.0:8000
 # gunicorn django_bike.wsgi:application --bind 0.0.0.0:8000
