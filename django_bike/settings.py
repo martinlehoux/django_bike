a7b17c0952bc1b66ccb425a276206f4492870dd6
Martin Lehoux <martin@lehoux.net>
2020-06-25 02:17:29 -0700

    feat - Use sendgrid for emails

--- django_bike/settings.py
+++ django_bike/settings.py
@@ -122,3 +122,14 @@ if DEBUG:
 if DEBUG:
     EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
     EMAIL_FILE_PATH = os.path.join(BASE_DIR, "mail")
+
+if DOCKER:
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_HOST = "smtp.sendgrid.net"
+    EMAIL_HOST_USER = "apikey"
+    EMAIL_HOST_PASSWORD = os.environ.get("SENDGRID_KEY")
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    SERVER_EMAIL = "martin@lehoux.net"
+
+ADMINS = [("Martin Lehoux", "martin@lehoux.net")]
