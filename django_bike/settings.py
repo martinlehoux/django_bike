38f9c6908c1ab56196b8939a872dbb826333f7c8
Martin Lehoux <martin@lehoux.net>
2020-07-01 08:02:46 -0700

    fix - Remove templates dir

--- django_bike/settings.py
+++ django_bike/settings.py
@@ -46,7 +46,7 @@ FORM_RENDERER = "django.forms.renderers.TemplatesSetting"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
