c1eedf1a2f933268afac832db3b7e008a45727c8
Martin Lehoux <martin@lehoux.net>
2020-07-02 01:37:27 -0700

    feat - Modal JS open and close

--- apps/main/static/js/index.js
+++ apps/main/static/js/index.js
@@ -5,3 +5,11 @@ document.querySelectorAll("input[type=file]").forEach(input => {
 document.querySelectorAll("button.delete").forEach(button => {
     button.onclick = () => button.parentNode.parentNode.removeChild(button.parentNode)
 })
+
+document.querySelectorAll("button[name=modal-open]").forEach(button => {
+    button.onclick = () => document.getElementById(button.value).classList.add("is-active")
+})
+
+document.querySelectorAll(".modal button[name=modal-close]").forEach(button => {
+    button.onclick = () => document.getElementById(button.value).classList.remove("is-active")
+})
