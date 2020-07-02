579d54aacf80cb4bd9342cf8099ea592db44dc5a
Martin Lehoux <martin@lehoux.net>
2020-07-02 03:59:06 -0700

    feat - Strava GPX parser

---
+++ apps/track/parsers/gpx/strava.py
@@ -0,0 +1,22 @@
+from xml.etree import ElementTree
+from typing import List
+
+from django.utils.dateparse import parse_datetime
+
+
+class StravaGPXParser:
+    def parse(self, file: bytes) -> List[dict]:
+        xml = ElementTree.parse(file)
+        root = xml.getroot()
+        points = []
+        for trkpt in root[1][2]:
+            points.append(
+                dict(
+                    lat=float(trkpt.attrib["lat"]),
+                    lon=float(trkpt.attrib["lon"]),
+                    alt=float(trkpt[0].text),
+                    dist=0.0,
+                    time=parse_datetime(trkpt[1].text),  # UTC
+                )
+            )
+        return points
