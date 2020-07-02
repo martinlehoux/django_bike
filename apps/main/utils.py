26b38e91bcf22a237da05356d9f7acab3041607a
Martin Lehoux <martin@lehoux.net>
2020-07-01 23:55:05 -0700

    fix - Ensure media-test exists

--- apps/main/utils.py
+++ apps/main/utils.py
@@ -8,6 +8,11 @@ from django.test import TestCase, override_settings
 @override_settings(MEDIA_ROOT="media-test/")
 class FileSystemTestCase(TestCase):
     @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        (Path(settings.BASE_DIR) / "media-test").mkdir(exist_ok=True)
+
+    @classmethod
     def tearDownClass(cls):
         super().tearDownClass()
         shutil.rmtree(Path(settings.BASE_DIR) / "media-test")
