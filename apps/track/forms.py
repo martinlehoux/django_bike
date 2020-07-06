164fd66712b40fcd25775cf617e91caafa0ba02a
Martin Lehoux <martin@lehoux.net>
2020-07-05 23:01:29 -0700

    feat - First work on date and time fields

--- apps/track/forms.py
+++ apps/track/forms.py
@@ -1,4 +1,5 @@
-from typing import List
+from typing import Any, List, Dict
+from datetime import datetime
 
 from django import forms
 
@@ -8,6 +9,10 @@ from . import tasks
 
 
 class TrackCreateForm(forms.ModelForm):
+    class Meta:
+        model = Track
+        fields = ["name", "source_file", "parser"]
+
     name = forms.CharField(
         max_length=128, required=True, strip=True, widget=TextListInput
     )
@@ -17,10 +22,6 @@ class TrackCreateForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.fields["name"].widget.attrs.update({"data_list": track_names})
 
-    class Meta:
-        model = Track
-        fields = ["name", "source_file", "parser"]
-
     def save(self, commit=True):
         self.instance.state = Track.StateChoices.PROCESSING
         track = super().save(commit=commit)
@@ -29,14 +30,30 @@ class TrackCreateForm(forms.ModelForm):
 
 
 class TrackEditForm(forms.ModelForm):
+    class Meta:
+        model = Track
+        fields = ["name", "public", "datetime"]
+
     name = forms.CharField(
         max_length=128, required=True, strip=True, widget=TextListInput
     )
+    # date = forms.DateField(localize=True)
+    # time = forms.TimeField(localize=True)
 
     def __init__(self, track_names: List[str], *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields["name"].widget.attrs.update({"data_list": track_names})
 
-    class Meta:
-        model = Track
-        fields = ["name", "public", "datetime"]
+    def get_initial_for_field(self, field: forms.Field, field_name: str):
+        # if field_name == "date":
+        #     return self.instance.datetime.date()
+        # if field_name == "time":
+        #     return self.instance.datetime.time()
+        return super().get_initial_for_field(field, field_name)
+
+    def clean(self) -> Dict[str, Any]:
+        cleaned_data = super().clean()
+        # cleaned_data["datetime"] = datetime.combine(
+        #     cleaned_data["date"], cleaned_data["time"]
+        # )
+        return cleaned_data
