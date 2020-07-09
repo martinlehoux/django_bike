cf81bfe27e2c3b40dc1c64565bb7631125dae4e0
Martin Lehoux <martin@lehoux.net>
2020-07-09 16:07:31 -0700

    feat - PermissionRequiredMethodMixin

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from typing import Dict, Union, Tuple
 
-# Create your views here.
+from django.core.exceptions import ImproperlyConfigured
+from rules.contrib.views import PermissionRequiredMixin
+
+
+class PermissionRequiredMethodMixin(PermissionRequiredMixin):
+    permission_required_map: Dict[str, Union[str, Tuple[str]]] = None
+
+    def get_permission_required(self):
+        if self.permission_required_map is not None:
+            if not isinstance(self.permission_required_map, dict):
+                raise ImproperlyConfigured(
+                    f"{self.__class__.__name__} has a wrong permission_required_map attribute. "
+                    "It should be a mapping from http method to permission."
+                )
+            perms = self.permission_required_map.get(self.request.method)
+            if perms is not None:
+                return (perms,) if isinstance(perms, str) else perms
+        return super().get_permission_required()
