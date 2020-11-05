fe28d736367b2a3edf24f54b1207af1448e23cc4
Martin Kagamino Lehoux <martin@lehoux.net>
2020-11-05 14:32:42 +0100

    fix - No r path for url

--- apps/notification/routing.py
+++ apps/notification/routing.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path(r"ws/notification/", consumers.NotificationConsumer),
+    path("ws/notification/", consumers.NotificationConsumer),
 ]
